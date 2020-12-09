0b1e6e139e91453dc1b8a6bed7150413ff59d73d
PedroPVergara <61695051+pedropa1@users.noreply.github.com>
2020-12-09 09:26:49 +0100

    Add files via upload

--- system_data_for_pyomo.py
+++ system_data_for_pyomo.py
@@ -1,15 +1,15 @@
 
 def system_data_for_pyomo(System_Data_Nodes, System_Data_Lines_Prima):
     # Network Data
-    On = [System_Data_Nodes.loc[i,'On'] for i in System_Data_Nodes.index]
-    Tb = {On[i]: System_Data_Nodes.loc[i,'Tb'] for i in System_Data_Nodes.index}
-    PDa = {On[i]: System_Data_Nodes.loc[i,'PDa'] for i in System_Data_Nodes.index}      
-    QDa = {On[i]: System_Data_Nodes.loc[i,'QDa'] for i in System_Data_Nodes.index} 
-    PDb = {On[i]: System_Data_Nodes.loc[i,'PDb'] for i in System_Data_Nodes.index}      
-    QDb = {On[i]: System_Data_Nodes.loc[i,'QDb'] for i in System_Data_Nodes.index} 
-    PDc = {On[i]: System_Data_Nodes.loc[i,'PDc'] for i in System_Data_Nodes.index}      
-    QDc = {On[i]: System_Data_Nodes.loc[i,'QDc'] for i in System_Data_Nodes.index}    
-    Ol = {(System_Data_Lines_Prima.loc[i,'FROM'],System_Data_Lines_Prima.loc[i,'TO']) for i in System_Data_Lines_Prima.index}
+    N = [System_Data_Nodes.loc[i,'N'] for i in System_Data_Nodes.index]
+    Tb = {N[i]: System_Data_Nodes.loc[i,'Tb'] for i in System_Data_Nodes.index}
+    PDa = {N[i]: System_Data_Nodes.loc[i,'PDa'] for i in System_Data_Nodes.index}      
+    QDa = {N[i]: System_Data_Nodes.loc[i,'QDa'] for i in System_Data_Nodes.index} 
+    PDb = {N[i]: System_Data_Nodes.loc[i,'PDb'] for i in System_Data_Nodes.index}      
+    QDb = {N[i]: System_Data_Nodes.loc[i,'QDb'] for i in System_Data_Nodes.index} 
+    PDc = {N[i]: System_Data_Nodes.loc[i,'PDc'] for i in System_Data_Nodes.index}      
+    QDc = {N[i]: System_Data_Nodes.loc[i,'QDc'] for i in System_Data_Nodes.index}    
+    L = {(System_Data_Lines_Prima.loc[i,'FROM'],System_Data_Lines_Prima.loc[i,'TO']) for i in System_Data_Lines_Prima.index}
     Raa_p = {(System_Data_Lines_Prima.loc[i,'FROM'],System_Data_Lines_Prima.loc[i,'TO']):System_Data_Lines_Prima.loc[i,'Raa_p'] for i in System_Data_Lines_Prima.index}
     Xaa_p = {(System_Data_Lines_Prima.loc[i,'FROM'],System_Data_Lines_Prima.loc[i,'TO']):System_Data_Lines_Prima.loc[i,'Xaa_p'] for i in System_Data_Lines_Prima.index}
     Rbb_p = {(System_Data_Lines_Prima.loc[i,'FROM'],System_Data_Lines_Prima.loc[i,'TO']):System_Data_Lines_Prima.loc[i,'Rbb_p'] for i in System_Data_Lines_Prima.index}
@@ -30,6 +30,6 @@ def system_data_for_pyomo(System_Data_Nodes, System_Data_Lines_Prima):
     Xcb_p = {(System_Data_Lines_Prima.loc[i,'FROM'],System_Data_Lines_Prima.loc[i,'TO']):System_Data_Lines_Prima.loc[i,'Xcb_p'] for i in System_Data_Lines_Prima.index}     
     Imax = {(System_Data_Lines_Prima.loc[i,'FROM'],System_Data_Lines_Prima.loc[i,'TO']):System_Data_Lines_Prima.loc[i,'Imax'] for i in System_Data_Lines_Prima.index}
         
-    Data_Network = [On, Ol, Tb, PDa, PDb, PDc, QDa, QDb, QDc, Raa_p, Xaa_p, Rbb_p, Xbb_p, Rcc_p, Xcc_p, Rab_p, Xab_p, Rac_p, Xac_p, Rbc_p, Xbc_p, Rba_p, Xba_p, Rca_p, Xca_p, Rcb_p, Xcb_p, Imax]
+    Data_Network = [N, L, Tb, PDa, PDb, PDc, QDa, QDb, QDc, Raa_p, Xaa_p, Rbb_p, Xbb_p, Rcc_p, Xcc_p, Rab_p, Xab_p, Rac_p, Xac_p, Rbc_p, Xbc_p, Rba_p, Xba_p, Rca_p, Xca_p, Rcb_p, Xcb_p, Imax]
     
     return Data_Network
